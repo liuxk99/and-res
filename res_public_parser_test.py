1e846b044e94c0b5daf4e0b2fcb4b86be893adfc
Thomas Liu <liuxk99@gmail.com>
2018-09-07 11:15:41 +0800

    <featue>: calculate sed id replacements.

--- res_public_parser_test.py
+++ res_public_parser_test.py
@@ -3,6 +3,7 @@ from unittest import TestCase
 
 import res_public_parser
 import test_const
+import utils
 
 
 class TestResPublicParser(TestCase):
@@ -27,13 +28,17 @@ class TestResPublicParser(TestCase):
     def test_eui(self):
         path = r"/home/thomas/Programmer/SDE/Android/letv/demeter/frameworks/base/core/res/res/values"
         path = r"C:\Users\thomas.XancL-NB\Desktop"
-        xml_file = r"public.eui.xml"
+        xml_file = r"public.xml"
 
         res_public_parser.parse(path + os.path.sep + xml_file)
         # utils.dumpSeq1(res_public_parser.publicList)
 
+        overlay_file = r"overlay.xml"
+        res_public_parser.parse(path + os.path.sep + overlay_file)
+        public_seq = res_public_parser.public_seq[:]
+
         dict = {u'key':'value'}
-        for pub in res_public_parser.public_seq:
+        for pub in public_seq:
             has = dict.has_key(pub._type)
             if has:
                 res_id = dict[pub._type]
@@ -48,4 +53,21 @@ class TestResPublicParser(TestCase):
         # res_public_parser.to_xml()
         # res_public_parser.to_res_ref(test_const.AndPackage)
 
+        res_public_parser.public_seq = []
+
+        eui_publc_xml_file = r"eui.public.xml"
+        res_public_parser.parse(path + os.path.sep + eui_publc_xml_file)
+        # utils.dumpSeq1(res_public_parser.publicList)
+
+        eui_public_seq = res_public_parser.public_seq
+        for pub in eui_public_seq:
+            old = pub._id
+            res_id = dict[pub._type]
+            pub._id = res_id + 1
+            dict[pub._type] = pub._id
+            new = pub._id
+
+            print('s/id="0x%08x"/id="0x%08x"/g' % (old, new))
+
+        # res_public_parser.to_xml()
         pass
